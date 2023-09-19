193c24959646c7d52defa69f989d8c4f3927083d
rahulcylsys841412 <140055218+rahulcylsys841412@users.noreply.github.com>
2023-09-19 11:29:16 +0530

    popup options

--- src/components/Molecules/NotificationOptions/NotificationOptions.js
+++ src/components/Molecules/NotificationOptions/NotificationOptions.js
@@ -1,14 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { NotificationOptionsStyle } from './NotificationOptionsStyle'
-import Entypo from 'react-native-vector-icons/Entypo'
+import { View, Text } from "react-native";
+import React from "react";
+import { NotificationOptionsStyle } from "./NotificationOptionsStyle";
+import Entypo from "react-native-vector-icons/Entypo";
 const NotificationOptions = () => {
   return (
-    <View>
-      <Text>Notification options</Text>
-      <Entypo name='cross'/>
-    </View>
-  )
-}
+    <>
+      <View>
+        <Text>Notification options</Text>
+        <Entypo name="cross" />
+      </View>
 
-export default NotificationOptions
+      <View>
+        <Text style={NotificationOptionsStyle.textoption}>Snooze this notification</Text>
+        <Text style={NotificationOptionsStyle.textoption}>Mute notification </Text>
+        <Text style={NotificationOptionsStyle.textoption}>Archive notification </Text>
+        <Text style={NotificationOptionsStyle.textoption}>Remove this notification</Text>
+      </View>
+    </>
+  );
+};
+
+export default NotificationOptions;
--- src/components/Molecules/NotificationOptions/NotificationOptionsStyle.js
+++ src/components/Molecules/NotificationOptions/NotificationOptionsStyle.js
@@ -2,4 +2,9 @@ import { StyleSheet } from "react-native";
 import { _COLORS,IMAGES,FONTFAMILY } from "../../../Themes";
 export const NotificationOptionsStyle = StyleSheet.create({
 
+    textoption:{
+        fontSize:14,
+        color:_COLORS.Kodie_BlackColor,
+        fontFamily:FONTFAMILY.K_SemiBold,
+    }
 });
