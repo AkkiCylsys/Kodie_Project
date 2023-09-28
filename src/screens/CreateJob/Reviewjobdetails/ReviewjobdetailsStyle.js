96265d59cbca3fe0d386c484200f853e3b4f04ad
HarshitaSoni2000 <harshita.soni@cylsys.com>
2023-09-28 18:59:17 +0530

    merge with rupesh

--- src/screens/CreateJob/Reviewjobdetails/ReviewjobdetailsStyle.js
+++ src/screens/CreateJob/Reviewjobdetails/ReviewjobdetailsStyle.js
@@ -2,9 +2,9 @@ import { StyleSheet } from "react-native";
 import { _COLORS, FONTFAMILY} from "../../../Themes/index";
 export const ReviewjobdetailsStyle = StyleSheet.create({
 
-Mainview: {
- flex:1,
- backgroundColor:_COLORS.Kodie_WhiteColor,
+  Mainview: {
+flex:1,
+backgroundColor:_COLORS.Kodie_WhiteColor,
 },
 img:{
     width: '100%', height: 200,resizeMode:"cover"
@@ -48,14 +48,6 @@ marginBottom:20
     marginTop: 10,
     borderRadius: 8,
   },
-  dropdown3: {
-    borderWidth: 1,
-    height: 40,
-    borderColor: _COLORS.Kodie_GrayColor,
-    marginTop: 10,
-    marginBottom:20,
-    borderRadius: 8,
-  },
 
   placeholderStyle: {
     fontSize: 14,
@@ -85,60 +77,5 @@ marginBottom:20
   file:{
     fontFamily: FONTFAMILY.K_Italic,
   },
-  pdfmainview:{
-    marginVertical:5
-  },
-  bindfile:{
-    flexDirection:'row',
-    alignItems:'center',
-    marginTop:10,
-    borderRadius: 6,
-    borderWidth: 1,
-    height: 50,
-    borderColor: _COLORS.Kodie_GrayColor,
-    color: "#333",
-    paddingLeft: 10,
-    fontFamily: FONTFAMILY.K_Medium,
-    },
-    pdfName: {
-      fontSize: 14,
-      color: _COLORS.Kodie_BlackColor,
-      fontFamily: FONTFAMILY.K_Bold,
-      marginHorizontal:5
-    },
-    pdfSize: {
-      fontSize: 12,
-      color: _COLORS.Kodie_MediumGrayColor,
-      fontFamily: FONTFAMILY.K_Medium,
-      marginHorizontal:5
-    },
-    doticon:{
-      color:_COLORS.Kodie_GrayColor,
-    },
-
-    textContainer: {
-      flexDirection: "column",
-      borderWidth:0.5,
-      marginHorizontal:15,
-      flex:1,
-      paddingHorizontal:10,
-      padding:10,
-      marginVertical:5,
-      borderRadius:10,
-      flexDirection:'row',
-      alignItems:'center',
-      justifyContent:'space-between'
-    },
-    Details_Tab: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      marginHorizontal: 16,
-      marginTop: 10,
-    },
-    Tab_text: {
-      fontSize: 11,
-      fontFamily: FONTFAMILY.K_SemiBold,
-      color: _COLORS.Kodie_BlackColor,
-    },
   
 });
