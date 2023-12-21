5c59996cc3a8cb594d0ede72b3e49620b795a53d
Deependra-jhariya <deependrajhariya@gmail.com>
2023-12-21 18:16:32 +0530

    updateMode in createJob
    Done

--- src/screens/CreateJob/CreateJobFirstScreen.js
+++ src/screens/CreateJob/CreateJobFirstScreen.js
@@ -711,8 +711,9 @@ export default CreateJobFirstScreen = (props) => {
         if (response.data.success === true) {
           setJobDetailsData(response.data.data);
           console.log("jobDetailsData....", response.data.data);
-          setSelectJobTypeid(response.data.data.job_type_key);
+          // setSelectJobTypeid(response.data.data.job_type_key);
           // alert(response.data.data.job_type_key);
+          setIsClick(parseInt(response.data.data.job_type_key))
           setAboutyourNeed(response.data.data.job_description);
           setservicesValue(
             parseInt(response.data.data?.job_service_you_looking_key)
@@ -721,8 +722,8 @@ export default CreateJobFirstScreen = (props) => {
           setProperty_value(parseInt(response.data.data?.property_type_key));
           setLocation(response.data.data?.job_location);
           setRatingThresholdValue(parseInt(response.data.data?.job_rating_key));
-          setlatitude(response.data.data?.location_latitude)
-          setlongitude(response.data.data?.location_longitude)
+          setlatitude(response.data.data?.location_latitude);
+          setlongitude(response.data.data?.location_longitude);
         } else {
           alert(response.data.message);
           setIsLoading(false);
@@ -1037,7 +1038,7 @@ export default CreateJobFirstScreen = (props) => {
                   latitude: latitude,
                   longitude: longitude,
                   JobId: JobId,
-                  editMode:editMode
+                  editMode: editMode,
                 })
               }
               _ButtonText={"Next"}
