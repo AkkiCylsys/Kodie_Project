22cad17375b7df9dc3b285f6d3af84298004ba1f
HarshitaSoni2000 <112847004+HarshitaSoni2000@users.noreply.github.com>
2023-12-22 17:47:08 +0530

    Biddingdetails
    changes

--- src/screens/Tenant/Jobs/JobDetails/BiddingDetails/Biddingdetails.js
+++ src/screens/Tenant/Jobs/JobDetails/BiddingDetails/Biddingdetails.js
@@ -1,87 +1,94 @@
-import React, { useState, useRef } from "react";
-import { View, ScrollView, Text, TouchableOpacity } from "react-native";
+import React, { useRef } from "react";
+import { View, FlatList, ScrollView } from "react-native";
 import { BiddingDetailsStyle } from "./BiddingDetailsStyle";
 import TopHeader from "../../../../../components/Molecules/Header/Header";
 import { _goBack } from "../../../../../services/CommonServices";
 import { _COLORS, IMAGES, LABEL_STYLES } from "../../../../../Themes";
 import DividerIcon from "../../../../../components/Atoms/Devider/DividerIcon";
-import CustomSingleButton from "../../../../../components/Atoms/CustomButton/CustomSingleButton";
-import SearchBar from "../../../../../components/Molecules/SearchBar/SearchBar";
-import CustomTabNavigator from "../../../../../components/Molecules/CustomTopNavigation/CustomTopNavigation";
-// import ContractorsComponent from "../../../../../components/Molecules/ContractorsComponent/ContractorsComponent";
 import BiddingComponent from "../../../../../components/Molecules/Bidding/BiddingComponent";
 import RBSheet from "react-native-raw-bottom-sheet";
 import ContractorsImage from "../../../../../components/Molecules/Contractors/ContractorsImage/ContractorsImage";
 
 const BiddingDetails = (props) => {
   const refRBSheet = useRef();
-  // const [activeScreen, setActiveScreen] = useState(false);
-  // const [activeTab, setActiveTab] = useState("Tab3");
-  // const [isLoading, setIsLoading] = useState(false);
-  // const toggleView = () => {
-  //   setVisible(!visible);
-  // };
 
-  // const checkTabs = () => {
-  //   switch (activeTab) {
-  //     case "Tab1":
-  //     case "Tab2":
-  //     case "Tab3":
-  //   }
-  // };
+  const biddingData = [
+    {
+      name: "Jason Stathom",
+      filedname: "Handyman",
+      fileddate: "8 Nov 2023",
+      startRating: "4.6",
+      ratingnumber: "231",
+      ratingamount: "Bid amount: ",
+      ratingprice: "$200",
+      address: "1234, Contractor’s address. Australia",
+      notverified: "Not verified",
+      CoverText1: "Cover letter -",
+      CoverText2:
+        "I am the best contractor in town, ready to go. Check my best works portfolio and...",
+      CoverText3: "read more",
+    },
+    {
+      name: "Mesut Ozil",
+      filedname: "Plumber",
+      fileddate: "8 Nov 2023",
+      startRating: "4.0",
+      ratingnumber: "100",
+      ratingamount: "Bid amount: ",
+      ratingprice: "$180",
+      address: "1234, Contractor’s address. Australia",
+      verified: true,
+      CoverText1: "Cover letter -",
+      CoverText2:
+        "I am the best contractor in town, ready to go. Check my best works portfolio and...",
+      CoverText3: "read more",
+    },
+    {
+      name: "Jack Black",
+      filedname: "Handyman",
+      fileddate: "8 Nov 2023",
+      startRating: "3.6",
+      ratingnumber: "231",
+      ratingamount: "Bid amount: ",
+      ratingprice: "$200",
+      address: "1234, Contractor’s address. Australia",
+      verified: true,
+      CoverText1: "Cover letter -",
+      CoverText2:
+        "I am the best contractor in town, ready to go. Check my best works portfolio and...",
+      CoverText3: "read more",
+    },
+    // Add more items as needed
+  ];
+
+  const renderBiddingItem = ({ item }) => (
+    <>
+      <BiddingComponent
+        name={item.name}
+        filedname={item.filedname}
+        fileddate={item.fileddate}
+        startRating={item.startRating}
+        ratingnumber={item.ratingnumber}
+        ratingamount={item.ratingamount}
+        ratingprice={item.ratingprice}
+        address={item.address}
+        notverified={item.notverified}
+        verified={item.verified}
+        CoverText1={item.CoverText1}
+        CoverText2={item.CoverText2}
+        CoverText3={item.CoverText3}
+      />
+      <DividerIcon />
+    </>
+  );
+
   return (
     <View style={BiddingDetailsStyle.mainContainer}>
-      <ScrollView>
-        <View style={BiddingDetailsStyle.container}>
-          <BiddingComponent
-            name="Jason Stathom"
-            filedname="Handyman"
-            fileddate="8 Nov 2023"
-            startRating="4.6"
-            ratingnumber="231"
-            ratingamount="Bid amount: "
-            ratingprice="$200"
-            address="1234, Contractor’s address. Australia"
-            notverified="Not verified"
-            CoverText1="Cover letter -"
-            CoverText2="I am the best contractor in town, ready to go. Check my best works portfolio and..."
-            CoverText3="read more"
-          />
-
-          <DividerIcon />
-          <BiddingComponent
-            name="Mesut Ozil"
-            filedname="Plumber"
-            fileddate="8 Nov 2023"
-            startRating="4.0"
-            ratingnumber="100"
-            ratingamount="Bid amount: "
-            ratingprice="$180"
-            address="1234, Contractor’s address. Australia"
-            verified={true}
-            CoverText1="Cover letter -"
-            CoverText2="I am the best contractor in town, ready to go. Check my best works portfolio and..."
-            CoverText3="read more"
-          />
-
-          <DividerIcon />
-          <BiddingComponent
-            name="Jack Black"
-            filedname="Handyman"
-            fileddate="8 Nov 2023"
-            startRating="3.6"
-            ratingnumber="231"
-            ratingamount="Bid amount: "
-            ratingprice="$200"
-            address="1234, Contractor’s address. Australia"
-            verified={true}
-            CoverText1="Cover letter -"
-            CoverText2="I am the best contractor in town, ready to go. Check my best works portfolio and..."
-            CoverText3="read more"
-          />
-          <DividerIcon />
-        </View>
-      </ScrollView>
+      <FlatList
+        data={biddingData}
+        renderItem={renderBiddingItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
       {/* <RBSheet
         ref={refRBSheet}
         height={200}
@@ -103,4 +110,5 @@ const BiddingDetails = (props) => {
     </View>
   );
 };
+
 export default BiddingDetails;
