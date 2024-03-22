29d9e608507ef26975b201629502d18c75014df8
HarshitaSoni2000 <harshita.soni@cylsys.com>
2024-03-22 16:43:26 +0530

    code push

--- src/screens/Landlord/Landlordprofile/CompanyDetails/CompanyDetails.js
+++ src/screens/Landlord/Landlordprofile/CompanyDetails/CompanyDetails.js
@@ -1004,7 +1004,7 @@ export default CompanyDetails = props => {
         <SearchPlaces
           onPress={(data, details = null) => {
             console.log('LocationData....', details);
-            if (tabValue == 'IndividualInProfile') {
+            if (tabValue === 'IndividualInProfile') {
               setlatitude(details.geometry.location.lat);
               setlongitude(details.geometry.location.lng);
             } else {
