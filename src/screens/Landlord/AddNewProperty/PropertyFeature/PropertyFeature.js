8d830ff67b6f4d0fa9819ba0dacba6c6796e5a18
Deependra-jhariya <deependrajhariya@gmail.com>
2024-08-23 11:06:06 +0530

    propertyFeature,PropertyReview
    Done

--- src/screens/Landlord/AddNewProperty/PropertyFeature/PropertyFeature.js
+++ src/screens/Landlord/AddNewProperty/PropertyFeature/PropertyFeature.js
@@ -118,6 +118,7 @@ export default PropertyFeature = props => {
         const yesFeatureId = apiAdditionalFeaturesIds.find(id => id == 71);
         setSelectedButtonFurnished(furnishedFeatureId);
         setSelectedButtonDeposit(yesFeatureId);
+        response?.data?.property_details[0]?.floor_size == 0?setFlorSize(''):
         setFlorSize(response?.data?.property_details[0]?.floor_size);
         const keyFeaturesId =
           response?.data?.property_details[0]?.additional_key_features_id;
@@ -127,7 +128,7 @@ export default PropertyFeature = props => {
             : keyFeaturesId;
         setAdditionalFeaturesKeyValue(
           Array.isArray(parsedKeyFeaturesId) ? parsedKeyFeaturesId : [],
-        );
+        );response?.data?.property_details[0]?.land_area == 0?setLandArea(''):
         setLandArea(response?.data?.property_details[0]?.land_area);
       } else {
         console.error('propertyDetail_error:', response?.data?.error);
