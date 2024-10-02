50c93545e4210d43a0f315d6bc23f7e3c1f037fb
HarshitaSoni2000 <112847004+HarshitaSoni2000@users.noreply.github.com>
2024-10-02 19:34:42 +0530

    code puch

--- src/components/Molecules/UploadImage/UploadImage.js
+++ src/components/Molecules/UploadImage/UploadImage.js
@@ -1,120 +1,220 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, Alert, Text, Linking, FlatList, TouchableOpacity, AppState } from 'react-native';
-import { _COLORS } from "../../../Themes";
+import React, { useState, } from "react";
+import { View,Alert, Text,PermissionsAndroid,Linking,Platform, Image, FlatList, TouchableOpacity } from "react-native";
+import { IMAGES, _COLORS } from "../../../Themes";
 import { UploadImageStyle } from "./UploadImageStyle";
+import Entypo from "react-native-vector-icons/Entypo";
+import ImagePicker from "react-native-image-crop-picker";
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { PERMISSIONS, RESULTS, request } from 'react-native-permissions';
-import ImagePicker from "react-native-image-crop-picker";
-
-const options = [
+import { PERMISSIONS, RESULTS, request, check, openSettings } from 'react-native-permissions';
+const data = [
   {
     id: '1',
-    label: 'Take photo',
-    icon: <Feather name="camera" size={20} color={_COLORS.Kodie_GreenColor} style={{ alignSelf: 'center' }} />,
+    Data: 'Take photo',
+    // Img: IMAGES.camera,
+    Img: (
+      <Feather
+        name="camera"
+        size={20}
+        color={_COLORS.Kodie_GreenColor}
+        resizeMode={'contain'}
+        style={{alignSelf: 'center'}}
+      />
+    ),
   },
   {
     id: '2',
-    label: 'Choose photo from library',
-    icon: <MaterialIcons name="add-photo-alternate" size={20} color={_COLORS.Kodie_GreenColor} style={{ alignSelf: 'center' }} />,
+    Data: 'Choose photo from library',
+    // Img: IMAGES.gallery,
+    Img: (
+      <MaterialIcons
+        name="add-photo-alternate"
+        size={20}
+        color={_COLORS.Kodie_GreenColor}
+        resizeMode={'contain'}
+        style={{alignSelf: 'center'}}
+      />
+    ),
   },
+  // {
+  //   id: "3",
+  //   Data: "Choose file from folder",
+  //   Img: IMAGES.Upload,
+  // },
 ];
 
-const UploadImageData = ({ ImageName }) => {
+
+const UploadImageData = (props) => {
+  const [imageName, setImageName] = useState("");
   const [image, setImage] = useState({});
-  const [isPermissionChecked, setIsPermissionChecked] = useState(false);
 
-  useEffect(() => {
-    const subscription = AppState.addEventListener('change', handleAppStateChange);
-    return () => {
-      subscription.remove();
-    };
-  }, []);
-
-  const handleAppStateChange = async (nextAppState) => {
-    if (nextAppState === 'active' && !isPermissionChecked) {
-      await checkPermissions();
-    }
-  };
-
-  const checkPermissions = async () => {
-    const cameraPermission = await request(PERMISSIONS.IOS.CAMERA);
-    const libraryPermission = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
-    
-    if (cameraPermission !== RESULTS.GRANTED || libraryPermission !== RESULTS.GRANTED) {
-      Alert.alert(
-        "Permissions Required",
-        "Please enable camera and photo library permissions from settings.",
-        [
-          { text: "Open Settings", onPress: openAppSettings },
-          { text: "Cancel", style: "cancel" },
-        ]
-      );
-    } else {
-      setIsPermissionChecked(true);
-    }
-  };
 
   const openAppSettings = () => {
-    Linking.openSettings();
-  };
-
-  const handleImageSelection = useCallback(async (item) => {
-    try {
-      let permissionStatus;
-      if (item.id === '1') {
-        permissionStatus = await request(PERMISSIONS.IOS.CAMERA);
-      } else if (item.id === '2') {
-        permissionStatus = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
-      }
-
-      if (permissionStatus === RESULTS.GRANTED) {
-        const selectedImage = item.id === '1'
-          ? await ImagePicker.openCamera({ width: 300, height: 400, cropping: true, compressImageQuality: 0.5 })
-          : await ImagePicker.openPicker({ width: 300, height: 400, cropping: true, compressImageQuality: 0.5 });
-
-        setImage(selectedImage);
-        ImageName(selectedImage);
-      } else {
-        showPermissionDeniedAlert();
-      }
-    } catch (err) {
-      console.warn("Error handling image selection:", err);
-      Alert.alert("Error", "An error occurred while selecting an image.");
+    if (Platform.OS === 'android') {
+      Linking.openSettings();
     }
-  }, [ImageName]);
-
-  const showPermissionDeniedAlert = () => {
-    Alert.alert(
-      "Permission Denied",
-      "Please enable permission from settings.",
-      [
-        { text: "Open Settings", onPress: openAppSettings },
-        { text: "Cancel", style: "cancel" },
-      ]
+  };
+ 
+ 
+  const UploadImageContent = ({ item, index }) => {
+    return (
+      <>
+        <TouchableOpacity
+          style={UploadImageStyle.content_View}
+          // onPress={Platform.OS == 'ios' ? handleImageSelection({item}) : handleAndroidPermission({item})}
+         onPress={async()=>
+          {
+            if(Platform.OS === 'ios'){
+              try {
+                // Request camera permission
+                const cameraPermission = await request(PERMISSIONS.IOS.CAMERA);
+                const photoPermission = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
+          
+                if (
+                  cameraPermission === 'denied' || 
+                  cameraPermission === 'blocked' || 
+                  photoPermission === 'denied' || 
+                  photoPermission === 'blocked'
+                ) {
+                  Alert.alert(
+                    'Permission Required',
+                    'We need access to your camera and photo library. Please enable it in the app settings.',
+                    [
+                      { text: 'Cancel', style: 'cancel' },
+                      { text: 'Open Settings', onPress: openSettings },
+                    ]
+                  );
+                  return; // Exit if permission is not granted
+                }
+          
+                // Proceed based on item.id
+                if (item.id === '1') {
+                  // Open camera
+                  const image = await ImagePicker.openCamera({
+                    width: 300,
+                    height: 400,
+                    cropping: true,
+                    compressImageQuality: 0.5,
+                  });
+                  setImage(image);
+                  setImageName(image?.path);
+                  props?.ImageName(image);
+                  console.log('Image from camera:', image);
+                } else if (item.id === '2') {
+                  // Open gallery
+                  const image = await ImagePicker.openPicker({
+                    width: 300,
+                    height: 400,
+                    cropping: true,
+                    compressImageQuality: 0.5,
+                  });
+                  setImage(image);
+                  setImageName(JSON.stringify(image?.path));
+                  props?.ImageName(image);
+                  console.log('Image from gallery:', image);
+                }
+              } catch (err) {
+                console.error('Error selecting image:', err);
+                // Alert.alert('Error', 'Failed to select image. Please try again.');
+              }
+            }else{
+              const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.CAMERA,
+                {
+                  title: 'Camera Permission',
+                  message:
+                    'This app needs camera access to take photos.',
+                  buttonNeutral: 'Ask Me Later',
+                  buttonNegative: 'Cancel',
+                  buttonPositive: 'OK',
+                },
+              );
+              if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                if (item.id === "1") {
+                  ImagePicker.openCamera({
+                    width: 300,
+                    height: 400,
+                    cropping: true,
+                    compressImageQuality: 0.5,
+                  })
+                    .then((image) => {
+                      // console.log('image....', image);
+                      setImage(image);
+                      setImageName(image?.path);
+                      // props?.ImageName(image?.path)
+                      props?.ImageName(image);
+            
+                      console.log("ImagePath..", imageName);
+                    })
+                    .catch((err) => {
+                      console.log("err...", err);
+                    });
+                }
+                if (item.id === "2") {
+                  // Navigate to Choose photo from library when Contact Us is clicked.......
+                  ImagePicker.openPicker({
+                    width: 300,
+                    height: 400,
+                    cropping: true,
+                    compressImageQuality: 0.5,
+                    // multiple: true,
+                  })
+                    .then((image) => {
+                      // console.log(image);
+                      setImage(image);
+                      setImageName(JSON.stringify(image?.path));
+                      props?.ImageName(image);
+                      console.log("ImagePath..", imageName);
+                    })
+                    .catch((err) => {
+                      console.log("err...", err);
+                    });
+                }
+              } else {
+                Alert.alert('Camera permission denied. Open settings to enable camera access.', '', [
+                  { text: 'Cancel', style: 'cancel' },
+                  { text: 'Open Settings', onPress: openAppSettings },
+                ]);
+              }
+            }
+           
+          }
+         }
+        >
+          <View style={UploadImageStyle.IconView}>{item.Img}</View>
+          <Text style={UploadImageStyle.text}>{item.Data}</Text>
+        </TouchableOpacity>
+      </>
     );
   };
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity
-      style={UploadImageStyle.content_View}
-      onPress={() => handleImageSelection(item)}
-    >
-      <View style={UploadImageStyle.IconView}>{item.icon}</View>
-      <Text style={UploadImageStyle.text}>{item.label}</Text>
-    </TouchableOpacity>
-  );
-
   return (
     <View style={UploadImageStyle.mainContainer}>
+      {/* <View style={UploadImageStyle.upload_View}>
+        <Text style={UploadImageStyle.uploadImgText}>
+          {props.heading_Text || "Upload image"}
+        </Text>
+        <TouchableOpacity>
+          <Entypo
+            name="cross"
+            size={25}
+            color={_COLORS.Kodie_BlackColor}
+            style={UploadImageStyle.crossIconStyle}
+          />
+        </TouchableOpacity>
+      </View> */}
+
       <FlatList
-        data={options}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
+        data={data}
+        scrollEnabled
         showsVerticalScrollIndicator={false}
+        contentContainerStyle={{}}
+        keyExtractor={(item) => item?.id}
+        renderItem={UploadImageContent}
       />
     </View>
   );
 };
 
 export default UploadImageData;
+
