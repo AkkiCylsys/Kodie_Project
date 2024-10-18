16df025847b3b3fa1de067ffae2badc1595f5c94
Deependra jhariya <107026547+Deependra-jhariya@users.noreply.github.com>
2024-10-18 18:10:41 +0530

    withNetworkConnectivity
    done

--- src/errorPages/NoInternet/withNetworkConnectivity.js
+++ src/errorPages/NoInternet/withNetworkConnectivity.js
@@ -1,47 +1,30 @@
-import React, {useEffect, useState} from 'react';
-import {View, Alert} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
 import NoInternet from './NoInternet'; // Import the NoInternet component
-
+ 
 const withNetworkConnectivity = WrappedComponent => {
   const WithNetworkConnectivity = props => {
     const [isConnected, setIsConnected] = useState(true);
-    const [isReachable, setIsReachable] = useState(true);
-
+ 
     useEffect(() => {
       const unsubscribe = NetInfo.addEventListener(state => {
-        const isInternetReachable = state.isInternetReachable;
-        setIsConnected(state.isConnected && isInternetReachable);
-        NetInfo.fetch('wifi').then(state => {
-          console.log('SSID', state.details.ssid);
-          console.log('BSSID', state.details.bssid);
-        });
-        if (!isInternetReachable) {
-          Alert.alert(
-            'No Internet Connection',
-            'You have lost your internet connection. Please check your network settings.',
-            [{text: 'OK'}],
-          );
-        } else {
-          setIsConnected(state.isConnected && isInternetReachable);
-        }
-        // setIsConnected(state.isConnected);
-        // setIsReachable(state.isInternetReachable);
+        setIsConnected(state.isConnected);
       });
-
+ 
       return () => {
         unsubscribe();
       };
     }, []);
-
+ 
     return (
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         {isConnected ? <WrappedComponent {...props} /> : <NoInternet />}
       </View>
     );
   };
-
+ 
   return WithNetworkConnectivity;
 };
-
+ 
 export default withNetworkConnectivity;
